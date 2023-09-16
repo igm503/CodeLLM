c5a754599f7dac58e4eddca71be780dc6abd29fa
igm503 <igm503@gmail.com>
2023-09-16 14:35:02 -0500

    adding script to build and run docker container

---
+++ run_docker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [[ "$(docker images -q codellm 2> /dev/null)" == "" ]]; then
+  echo "Docker image does not exist. Building now..."
+  docker build -t my-streamlit-app $SCRIPT_DIR
+else
+  echo "Docker image exists. Skipping build."
+fi
+
+docker run -v $SCRIPT_DIR:/app -p 8080:8080 codellm
